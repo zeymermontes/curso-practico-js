301da49275d990e4ec812e35ff22fcf09ae54f97
zeymermontes <zeymermontes@gmail.com>
2022-02-05 00:49:03 -0600

    se creo la respuesta en html con el resultado

--- descuentos.js
+++ descuentos.js
@@ -8,6 +8,25 @@ function calcularPrecioConDescuento(precio,descuento){
     return precioConDescuento;
 }
 
+function onClickCalculate() {
+    
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+    console.log("entra a onclick")
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const resultPrice = document.getElementById("resultPrice");
+    if (priceValue <= 0){
+        resultPrice.innerText = "Introduce un precio mayor a 0"; 
+    }else if (discountValue <= 0){
+        resultPrice.innerText = "El precio con un 0% de descuento es: $"+ precioConDescuento; 
+    }else{
+        resultPrice.innerText = "El precio con un " + discountValue + "%  de descuento es: $"+ precioConDescuento; 
+    }
+}
 
 // console.log({
 //     precioOriginal,
