9ac8f21f6ec2683c60d4ab11e6208ae10cbbd161
zeymermontes <zeymermontes@gmail.com>
2022-02-10 13:41:02 -0600

    se creo el JS para media y mediana

---
+++ mediana.js
@@ -0,0 +1,27 @@
+
+function calcularMediaAritmetica(lista){
+
+    const sumaLista = lista.reduce((valorAcumulado = 0, nuevoElemento) => {return valorAcumulado + nuevoElemento})
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+function calcularMediana(lista){
+
+    
+    let mediana;
+    let posicion = parseInt((lista.length)/2);
+    
+
+    if ( lista.length % 2 == 0){
+        const elem2 = lista[posicion];
+        const elem1 = lista[posicion - 1];
+
+        mediana = calcularMediaAritmetica([elem1,elem2])
+        return mediana;
+    }else{
+        mediana = lista[posicion];
+        return mediana;
+    }
+}
